439b137ca6c9655255af4523c1cc29ee4c9acf7e
Giang <nhogiang03tg@gamil.com>
2022-06-11 14:17:50 +0700

    update

--- Src/main.js
+++ Src/main.js
@@ -1,5 +1,6 @@
 const main=document.getElementById('main');
 const button=document.getElementById('button');
 button.addEventListener('click',function(){
+  console.log('object');
   main.classList.toggle("hidden");
 });
